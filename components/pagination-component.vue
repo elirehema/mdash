2513d3eb07fa1a548818ed41bbbc451a07c6ca0e
elirehema <rocksandystone@gmail.com>
2022-10-07 18:13:56 +0300

    Update pagination

--- components/pagination-component.vue
+++ components/pagination-component.vue
@@ -1,6 +1,23 @@
 <template>
   <div v-if="pages>1">
-    <div class=" min-w-full bg-emerald-50 py-5">
+    <div class="flex flex-col items-center p-4 ">
+      <!-- Help text -->
+      <span class="text-sm text-gray-700 dark:text-gray-400">
+        Showing <span class="font-semibold text-gray-900 dark:text-white">{{ page }}</span> out of <span class="font-semibold text-gray-900 dark:text-white">{{ pages }}</span> Pages
+      </span>
+      <div class="inline-flex mt-2 xs:mt-0">
+        <!-- Buttons -->
+        <button :disabled="page == 1" class="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-plainblue rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" @click="previousclicked">
+          <svg aria-hidden="true" class="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd" /></svg>
+          Prev
+        </button>
+        <button :disabled="page == pages" class="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-primary rounded-r border-0 border-l border-gray-700 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" @click="nextclicked">
+          Next
+          <svg aria-hidden="true" class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
+        </button>
+      </div>
+    </div>
+    <div v-if="false" class=" min-w-full bg-emerald-50 py-5">
       <div class="flex justify-end  w-full">
         <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
           <a class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20" @click="previousclicked">
